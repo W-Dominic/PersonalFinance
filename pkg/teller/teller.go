425895f5ffbd2d7e8b14406e33e62085886d286b
W-Dominic <dom0_o@protonmail.com>
2024-03-03 20:27:20 -0500

    working on http

--- pkg/teller/teller.go
+++ pkg/teller/teller.go
@@ -1,13 +1,38 @@
 package teller
 
+import (
+	"net/http"
+)
+
 type TellerAPI struct {
-	appId string
-	token string
+	appId    string
+	token    string
+	baseUrl  string
+	certPath string
+	keyPath  string
 }
 
-func Init(appId string, token string) *TellerAPI {
+func Init(appId string, token string, baseUrl string, certPath string, keyPath string) *TellerAPI {
 	return &TellerAPI{
-		appId: appId,
-		token: token,
+		appId:    appId,
+		token:    token,
+		baseUrl:  baseUrl,
+		certPath: certPath,
+		keyPath:  keyPath,
 	}
+}
+
+func (t *TellerAPI) GetAccounts() error {
+	return nil
+}
+
+func (t *TellerAPI) DoTellerHttp(method string, path string) error {
+    url := t.baseUrl + path
+
+    _, err := http.NewRequest(method, url, nil)
+    if err != nil {
+        return err
+    }
+
+    return nil
 }
--- pkg/teller/teller_test.go
+++ pkg/teller/teller_test.go
@@ -6,9 +6,12 @@ import (
 )
 
 func Test_TellerAPI(t *testing.T) {
-	
-    tellerApi := Init("fake id", "fake token")
+
+	tellerApi := Init("fake id", "fake token", "https://api.teller.io", "/path/to/cert", "/path/to/key")
 
 	assert.Equal(t, "fake id", tellerApi.appId)
 	assert.Equal(t, "fake token", tellerApi.token)
+	assert.Equal(t, "https://api.teller.io", tellerApi.baseUrl)
+	assert.Equal(t, "/path/to/cert", tellerApi.certPath)
+	assert.Equal(t, "/path/to/key", tellerApi.keyPath)
 }
